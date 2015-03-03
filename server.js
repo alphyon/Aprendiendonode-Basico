45c45501769be8d362614475a0f14208e316ba98
Alphyon <alphyon21@yahoo.es>
2015-03-02 21:29:02 -0600

    creando modulo y agregando index.js

--- server.js
+++ server.js
@@ -2,13 +2,17 @@
 
 var http = require('http');
 
-//creando el server -- definimos un callback
-function onRequest(request,response){
-  console.log('Petición Recibida');
-  response.writeHead(200,{'Content-Type' : 'text/html' });
-  response.write('Server de aprendizaje version 0.1');
-  response.end();
+//creando el server -- convirtiendo en modulo
+function iniciar(){
+  function onRequest(request,response){
+    console.log('Petición Recibida');
+    response.writeHead(200,{'Content-Type' : 'text/html' });
+    response.write('Server de aprendizaje version 0.1');
+    response.end();
+  }
+
+  http.createServer(onRequest).listen(8888);
+  console.log('Servidor Iniciado');
 }
 
-http.createServer(onRequest).listen(8888);
-console.log('Servidor Iniciado');
+exports.iniciar = iniciar;
