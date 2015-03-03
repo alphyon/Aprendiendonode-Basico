27ff8c469169e8c3dd156986c2c120f5c9f70b5e
Alphyon <alphyon21@yahoo.es>
2015-03-02 21:09:35 -0600

    refactorizando el server

--- server.js
+++ server.js
@@ -2,9 +2,11 @@
 
 var http = require('http');
 
-//creando el server
-http.createServer(function(request,response){
-    response.writeHead(200,{'Content-Type' : 'text/html' });
-    response.write('Server de aprendizaje version 0.1');
-    response.end();
-}).listen(8888);
+//creando el server -- refactorizamos
+function onRequest(request,response){
+  response.writeHead(200,{'Content-Type' : 'text/html' });
+  response.write('Server de aprendizaje version 0.1');
+  response.end();
+}
+
+http.createServer(onRequest).listen(8888);
