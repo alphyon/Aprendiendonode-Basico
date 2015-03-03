da6a844560ffd9aeb8f232b0754ff656c36fcbfe
Alphyon <alphyon21@yahoo.es>
2015-03-02 21:18:14 -0600

    agregando callbacks -- mensajes que se muestran al hacer peticion

--- server.js
+++ server.js
@@ -2,11 +2,13 @@
 
 var http = require('http');
 
-//creando el server -- refactorizamos
+//creando el server -- definimos un callback
 function onRequest(request,response){
+  console.log('Petición Recibida');
   response.writeHead(200,{'Content-Type' : 'text/html' });
   response.write('Server de aprendizaje version 0.1');
   response.end();
 }
 
 http.createServer(onRequest).listen(8888);
+console.log('Servidor Iniciado');
